66f49f32421a41f0338106e246b55c08bb284c21
OleksanderFrontend <pozitiv4ikc@gmail.com>
2021-08-03 20:42:50 +0300

    changed images folder

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,8 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 3000,
     fade: true,
-    prevArrow: '<button type = "button" class = "slick-prev"><img src="../images/arrow-left.svg" alt="Левая стрелка"></button>',
-    nextArrow: '<button type = "button"class = "slick-next"><img src="../images/arrow-right.svg" alt="Правая стрелка"></button>',
+    prevArrow: '<button type = "button" class = "slick-prev"><img src="../images/icons/arrow-left.svg" alt="Левая стрелка"></button>',
+    nextArrow: '<button type = "button"class = "slick-next"><img src="../images/icons/arrow-right.svg" alt="Правая стрелка"></button>',
   });
 
   // $('.tab').on('click', function (e) {
