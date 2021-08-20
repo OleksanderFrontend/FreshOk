f3e4b733213e3152677d6bf9afb3b8929fe787a5
OleksanderFrontend <pozitiv4ikc@gmail.com>
2021-08-20 22:31:38 +0300

    Дз 13

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,39 @@
 $(function () {
-  $('.top-slider').slick({
-    autoplay: true,
-    autoplaySpeed: 3000,
+  $('.sale__slider').slick({
+    // autoplay: true,
+    // autoplaySpeed: 3000,
     fade: true,
     prevArrow: '<button type = "button" class = "slick-prev"><img src="../images/icons/arrow-left.svg" alt="Левая стрелка"></button>',
     nextArrow: '<button type = "button"class = "slick-next"><img src="../images/icons/arrow-right.svg" alt="Правая стрелка"></button>',
   });
 
-  // $('.tab').on('click', function (e) {
-  //   e.preventDefault();
-
-  //   $('.tab').removeClass('tab--active');
-  //   $('.tab-content').removeClass('tab-content--active');
-
-  //   $(this).addClass('tab--active');
-  //   $($(this).attr('href')).addClass('tabs-content--active');
+  // $('.brands__items').slick({
+  //   slidesToShow: 6
   // });
 
-  $('.tabs-wrapper').each(function () {
-    let ths = $(this);
-    ths.find('.tab-item').not(':first').hide();
-    ths.find('.tab').on('click', function () {
-      ths.find('.tab').removeClass('active').eq($(this).index()).addClass('active');
-      ths.find('.tab-item').hide().eq($(this).index()).fadeIn()
-    }).eq(0).addClass('active');
-  });
+  // var mixer = mixitup('.top-products__сards');
+  // var mixer = mixitup('.promotions__cards');
 });
+
+ var containerEl1 = document.querySelector('[data-ref="mixer-1"]');
+ var containerEl2 = document.querySelector('[data-ref="mixer-2"]');
+
+ var config = {
+   controls: {
+     scope: 'local'
+   }
+ };
+
+ var mixer1 = mixitup(containerEl1, config);
+ var mixer2 = mixitup(containerEl2, config);
+
+
+
+//   $('.tabs-wrapper').each(function () {
+//     let ths = $(this);
+//     ths.find('.tab-item').not(':first').hide();
+//     ths.find('.tab').on('click', function () {
+//       ths.find('.tab').removeClass('active').eq($(this).index()).addClass('active');
+//       ths.find('.tab-item').hide().eq($(this).index()).fadeIn()
+//     }).eq(0).addClass('active');
+//   });
